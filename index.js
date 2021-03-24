63055f348a9fe9fe335c4436c65aa41f7243a9e4
Adam cooke <adam-cooke@live.co.uk>
2021-03-24 17:05:41 +0000

    v1.0.1 - Added copy to clipboard functionality - overlay for 'keyword Packs' - overall styling adjustments

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const locations = document.querySelector(".locations");
 const select = document.querySelector("select");
 const main = document.querySelector(".main");
 const offer = document.querySelector(".offer");
+const copy = document.querySelector(".copy");
+const packs = document.querySelector(".packs");
+const packOverlay = document.querySelector(".packOverlay");
+const exitOverlay = document.querySelector(".exit-overlay");
 
 const offerValues = [
   "sale",
@@ -17,7 +21,13 @@ const offerValues = [
   "offer",
 ];
 
-const locationValues = ["near", "near my location", "near me", "my area"];
+const locationValues = [
+  "near",
+  "near my location",
+  "near me",
+  "my area",
+  "in my area",
+];
 
 const loopArray = () => {
   const HTML = offerValues.map((element) => {
@@ -35,5 +45,28 @@ const loopLocationArray = () => {
   main.innerHTML = HTML;
 };
 
+const copyMyText = () => {
+  let text = main.textContent;
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      alert("Text copied to clipboard");
+    })
+    .catch((err) => {
+      alert("Error in copying text: ", err);
+    });
+};
+
 deals.addEventListener("click", loopArray);
 locations.addEventListener("click", loopLocationArray);
+copy.addEventListener("click", copyMyText);
+
+packs.addEventListener("click", () => {
+  packOverlay.classList.toggle("packOverlayShow");
+  console.log("click");
+});
+
+exitOverlay.addEventListener("click", () => {
+  packOverlay.classList.toggle("packOverlayShow");
+  console.log("click");
+});
